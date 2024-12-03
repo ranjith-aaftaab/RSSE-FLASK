d0996824044a29d160f3b84ca0f7ee311f6eb86c
RANJITH-SIST <127429021+ranjith-aaftaab@users.noreply.github.com>
2024-12-03 17:52:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -128,4 +128,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create the database tables
         print("Database created successfully!")
-    app.run(debug=True)
+    app.run()
